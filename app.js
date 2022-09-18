ea24f34922369b78d38b09ea16cc56cb66ba4602
dev0-arslan <arslanansarii947@gmail.com>
2022-09-18 11:15:31 -0700

    Assignment No-06

---
+++ app.js
@@ -0,0 +1,96 @@
+// 1. Write a ts program to print all natural numbers from 1 to n. - using while loop
+// var num: any = prompt("enter value of n")
+// var n: number = parseInt(num);
+// var n1: number = 0;
+// while (n1 <= n) {
+//     n1++;
+//     console.log(n1);
+// }
+// 2. Write a ts program to print all natural numbers in reverse(from n to 1). - using while loop
+// var num:any = prompt("Enter value of n");
+// var n:number = parseInt(num);
+// while (0 < n) {
+//     console.log("whileReverse",n);
+//     n--;
+// }
+// 3. Write a ts program to print all alphabets from a to z. - using while loop.
+// var ch:any = 'a';
+// while (ch <= 'z') {
+//     ch++;
+//     console.log(ch);
+// }
+// 4. Write a ts program to print all even numbers between 1 to 100. - using while loop
+// var i = 1;
+// while (i <= 100) {
+//     if (i % 2 == 0) {
+//         console.log("number is even",i);
+//     }
+//     i++;
+// }
+// 4. Write a ts program to print all odd numbers between 1 to 100. - using while loop
+// var i = 1;
+// while (i <= 100) {
+//     if (i % 2 == 1) {
+//         console.log("number is odd",i);
+//     }
+//     i++;
+// }
+//5. Write a ts program to print all odd number between 1 to 100.
+// var num : number = 0;
+// console.log("all odd number are follow there under 100");
+// while(num<=100){
+//     if(num%2==1){
+//         console.log(num);
+//     }
+//     num++
+// }
+// 6. Write a ts program to find sum of all natural numbers between 1 to n.
+// var num:any = prompt("enter a positive number")
+// var n:any = parseInt(n)
+// var i:number = 0;
+// var sum:number = 0;
+// while (i<=n) {
+//     sum = sum +i;
+//     i++;
+// }
+// console.log("total of natural number between 'n' is", sum);
+//7. Write a ts program to find sum of all even numbers between 1 to n.
+// var num : any = prompt("enter any possitive number");
+// var n : number = parseInt(num);
+// var sum :number =0;
+// for(var i:number=1;i<=n;i++){
+//     if(i%2==0){
+//         sum=sum+i;
+//     }
+// }
+// console.log('sum of all even b/t ',n,"is",sum);
+// 9. Write a ts program to print multiplication table of any number.
+// var number: any = prompt('Enter an integer: ');
+// for (let i = 1; i <= 10; i++) {
+//     const result = i * number;
+//     console.log(`${number} * ${i} = ${result}`);
+// }
+//10. Write a ts program to count number of digits in a number.
+// var num : any =prompt("enter a number to know how many digit init")
+// var n : number = num.length;
+// console.log("the length of number is ",n);
+//11. Write a ts program to find first and last digit of a number.
+// var num : any = prompt("enter any positive number")
+// console.log("first digit" ,num[0]);
+// var n :any = num.charAt(num.length-1)
+// console.log("the last digit",n);
+//12. Write a ts program to find sum of first and last digit of a number.
+// var num : any = prompt("enter any positive number")
+// console.log("first digit" ,num[0]);
+// var n :any = num.charAt(num.length-1)
+// console.log("the last digit",n);
+// var sum : number =n+ num.[0]
+// 45. Write a ts program to convert Decimal to Binary number system.
+// var input = Number(prompt("Enter Decimal Number"));
+// console.log(input, "Convert Decimal to Binary:", input.toString(2));
+// 46. Write a ts program to convert Decimal to Octal number system.
+// var input = Number(prompt("Enter Decimal Number"));
+// console.log(input, "Convert Decimal to Octal:", input.toString(8));
+// 46. Write a ts program to convert Decimal to Hexadecimal number system.
+// var input = Number(prompt("Enter Decimal Number"));
+// console.log(input, "Convert Decimal to Hexadecimal:", input.toString(16));
